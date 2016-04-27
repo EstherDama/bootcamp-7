aecfb6d436b56af88bae92ae11d89e9635bd3bb6
Esther Dama <esther.dama@gmail.com>
2016-04-27 14:47:58 +0300

    The args and kwargs assignment was left behind

---
+++ day_3/assignment(args&kwargs).py
@@ -0,0 +1,22 @@
+def assignment (*args):
+	for i in args :
+		total += i
+	return total
+	return "{}: {}, ".format(label, value)
+
+#assignment again
+f = [(10, 20, 30), (10, 40) , (4, 5, 20)] 
+
+'''
+x: 10, y: 20, z: 40
+x: 10, y: 40
+...
+'''
+#l = [x, y, z]
+
+for item in f:
+	#length = len(item)
+	#b = str(item)
+	for n in item: 
+		print assignment('x',n)
+	print (''),
