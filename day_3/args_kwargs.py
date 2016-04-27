243566e5fc4690945dc0204c4a7b07f3053c6231
Esther Dama <esther.dama@gmail.com>
2016-04-27 14:45:34 +0300

    This is an update after kwargs and args

--- day_3/args_kwargs.py
+++ day_3/args_kwargs.py
@@ -24,3 +24,52 @@ for i in people:
 #and yet another way
 #for i in people:
 	#print hello(i[0],i[1])
+
+#overloading of parameters in Java
+#changes to a list the * arguements
+def super_sum(a, b, *args):
+	'''
+	Takes in variable number of items,
+	and returns the sum.
+	e.g. super_sum(10, 20) = 30
+		 super_sum(10, 20, 40) = 70
+		 super_sum (1, 4, 5, 6, 7) = 23
+	'''
+	total = 0
+
+	for i in args :
+		total += i
+	return total + a + b
+
+print super_sum(10,20)
+print super_sum(1, 4, 5, 7)
+a = [10, 40, 60]
+print super_sum(*a)  
+
+
+
+
+
+#now for kwargs
+#changes the list of parameters to dictonary
+# To unpack a dictonary **
+def hello_again (**kwargs): 
+	return "I am {}, and I'm {}". format(kwargs['name'], kwargs['age'])
+
+#test the kwargs
+print hello_again(name='Joe', age=20)
+print hello_again(age=20, name='Jane')
+
+other_people = [
+				{'name':'Joe', 'age': 98},
+				{'name':'Jane', 'age': 60},
+				{'name':'Trump', 'age': 23}
+				]
+
+joe = {'name':'Joe', 'age': 98}
+
+print hello_again(**joe) #this is the same as the line below
+print hello_again(name = 'Joe', age = 98)
+
+for person in other_people:
+	print hello_again(**person)
