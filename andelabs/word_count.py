05f0a354ad497c43469ec66d470f5e857ece21df
Esther Dama <esther.dama@gmail.com>
2016-04-29 08:56:36 +0300

    this is for word count changes-- it finally works

--- andelabs/word_count.py
+++ andelabs/word_count.py
@@ -1,36 +1,21 @@
+def words(string):
+  new_list = string.split()
+  string_dict = {}
 
-# def words(word):
-# 	for c in word:
-# 		if c.isspace():
-# 			string_list = word.split()
+  for word in new_list:
+    if word.isdigit(): 
+      word = int(word)
 
-# 	string_dict = {}
-
-# 	for word in string_list:
-# 		if word in string_dict:
-# 			string_dict[word] = string_dict[word] + 1
-# 		else:
-# 			string_dict[word] = 1
-# 	return string_dict
-
-
-
-# dict = word_count(string)
-# print(dict)
-
-#the one that works with one error
-def words(word):
-	string_list = word.split()
-	string_dict = {}
-
-	for word in string_list:
-		if word in string_dict:
-			string_dict[word] +=  1
-		else:
-			string_dict[word] = 1
-	return string_dict
+    if word in string_dict:
+      string_dict[word] +=  1
+      
+    else:
+      string_dict[word] = 1
+  return string_dict
 
 
+
+print words("Hi hi hi how are : : yah")
 string = "olly olly in come come free"
 print words(string)
 print words('go Go GO')
